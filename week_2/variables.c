178adc4ec231a3204aba8d339175dae1bd7283f3
hashaaerdene <99777293+hashaaerdene@users.noreply.github.com>
2022-02-23 11:04:40 +0800

    Create variables.c

---
+++ week_2/variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int my_phone_number = 88248811;
+    printf("my phone number is d.\n", my_phone_number);
+    
+    double myHeight = 1.6 ; 
+    printf("My height is %lf\n", myHeight);
+
+    char myLetter = 'j';    
+    printf("My letter is %c.\n", myLetter);
+   
+   char myName[] = "hashaa";  
+    printf("My name is %s.\n", myName);   
+
+    return 0;       
+}
