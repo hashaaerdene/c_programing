5f70bb907f9ab5cc876db9747f473fd2775f8b99
hashaaerdene <99777293+hashaaerdene@users.noreply.github.com>
2022-04-13 10:57:24 +0800

    Create switch.c

---
+++ week_9/switch.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+       char letter = 'a'; 
+     
+     
+     if (letter == 'a') { 
+         printf("apple\n"); 
+     } else if (letter == 'b') { 
+         printf("banana\n"); 
+     } else if (letter == 'c') { 
+         printf("cat\n"); 
+     } else { 
+         printf("other\n"); 
+     }
+     
+     return 0; 
+}
